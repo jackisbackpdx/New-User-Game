037fbb2851838bc3aff59f444f294ab367768ca3
Jackson Kelley <jackson.kelley.g@gmail.com>
2020-01-30 19:36:47 -0800

    getting setup for the Portland User Game, home page is complete, along with main API and user saving function

---
+++ user-get-set.js
@@ -0,0 +1,13 @@
+function saveUser(user) {
+    const json = JSON.stringify(user);
+    localStorage.setItem('user', json);
+}
+
+function getUser() {
+    const json = localStorage.getItem('user');
+    if(!json) return null;
+    const user = JSON.parse(json);
+    return user;
+}
+
+export { saveUser, getUser };
